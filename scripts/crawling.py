6d816a1ae011873f3b627024ff45b0bff42d1126
Stefano Carpita <carpitastefano@gmail.com>
2018-03-27 19:17:32 +0200

    updated crawling.py

--- scripts/crawling.py
+++ scripts/crawling.py
@@ -2,6 +2,7 @@ import sys
 import os
 import jsonpickle
 import tweepy
+import time
 
 CONSUMER_KEY='NHsKGfxrXTXlf2mfH2n0jbW1l'
 CONSUMER_SECRET = 'W0HE0cTlfIcJtkIX5hClcH4ILgyv018Q8fWdo0sgRo5bdFzAMA'
@@ -12,15 +13,15 @@ from tweepy import OAuthHandler
 
 import json as json
  
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-
-auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
- 
-api = tweepy.API(auth)
+##auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+#auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET) 
+##api = tweepy.API(auth)
 
 ###########################################################
 #Switching to application authentication
 auth = tweepy.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+
 
 #Setting up new api wrapper, using authentication only
 api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
@@ -41,27 +42,65 @@ searchQuery = "#facebookgate OR \
 
 ###########################################################
 
+##items = tweepy.Cursor(api.search, q='cambridgeanalytica',
+##                      since='2018-03-15').items(100)
+
+##prova =[item for item in items]
+
+###########################################################
+
 #Maximum number of tweets we want to collect 
-maxTweets = 100000
+maxTweets = 1000000
 
 #The twitter Search API allows up to 100 tweets per query
 tweetsPerQry = 100
-
 tweetCount = 0
 
 #Open a text file to save the tweets to
-with open('risultati_15_18_100k.json', 'w') as f:
 
+import time
+
+start_time = time.ctime()
+start=time.time()
+print "Started crawling at:"
+print start_time
+os.system("gzip Data/*.json")
+
+f=open('Data/risultati_15_18_{}.json'.format(tweetCount), 'w')
     #Tell the Cursor method that we want to use the Search API (api.search)
     #Also tell Cursor our query, and the maximum number of tweets to return
-    for tweet in tweepy.Cursor(api.search,q=searchQuery, since='2018-03-15',until='2018-03-18').items(maxTweets) :         
+for tweet in tweepy.Cursor(api.search,q=searchQuery, since='2018-03-15',until='2018-03-18').items(maxTweets) :         
 
-        #Write the JSON format to the text file, and add one to the number of tweets we've collected
-        f.write(jsonpickle.encode(tweet._json, unpicklable=False) + '\n')
-        tweetCount += 1
+    #Write the JSON format to the text file, and add one to the number of tweets we've collected
+    f.write(jsonpickle.encode(tweet._json, unpicklable=False) + '\n')
+    tweetCount += 1
 
-    #Display how many tweets we have collected
-    print("Downloaded {0} tweets".format(tweetCount))
+    if(tweetCount%1000==0):
+        f.close()
+            
+        print(".. downloaded tweets: {}".format(tweetCount))
+        print("running_time: {} minutes".format( (time.time()-start)/60))
+
+
+        os.system("gzip Data/*.json")
+
+        
+        f=open('Data/risultati_15_18_{}.json'.format(tweetCount), 'w')
+        
+
+        print("now sleeping zzz")        
+        time.sleep(22)
+
+        print("running again")
+
+
+            
+            
+
+        
+
+#Display how many tweets we have collected
+print("Downloaded {0} tweets".format(tweetCount))
 
 ###########################################################
 
