f83e3cb1f7f56a09754646537f7d1eba66243af7
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2018-06-05 10:05:01 +0200

    Updated community script with saving results functionality

--- scripts/community_discovery/comdis.py
+++ scripts/community_discovery/comdis.py
@@ -8,7 +8,7 @@ from networkx.algorithms import community
 from nf1 import NF1
 
 
-def apply_gn(g, ntimes=5):
+def apply_gn(g, ntimes=5, subsize=1000):
     print 'COMPUTING GIRVAN-NEWMAN SCORE FOR ' + str(ntimes) + ' ITERATIONS'
 
     results = dict()
@@ -20,22 +20,25 @@ def apply_gn(g, ntimes=5):
         coms_gn = [tuple(x) for x in next(gn_hierarchy)]
         results[i] = pquality.pquality_summary(g, coms_gn)
 
+    write_results('girvan_newman', subsize, results)
     return results
 
 
-def apply_kclique(g):
+def apply_kclique(g, subsize=1000):
     g = g.to_undirected()
     k = int(raw_input('COMPUTING K-CLIQUE SCORE FOR K: '))
     kclique = list(community.k_clique_communities(g, k))
     kclique = [tuple(x) for x in kclique]
 
     try:
+        write_results('k_clique', subsize,
+                      pquality.pquality_summary(g, kclique), k=k)
         return pquality.pquality_summary(g, kclique)
     except ValueError:
         return 'THERE ARE NO ' + str(k) + '-CLIQUES'
 
 
-def apply_louvain(g):
+def apply_louvain(g, subsize=1000):
     print 'COMPUTING LOUVAIN SCORE'
 
     g = g.to_undirected()
@@ -47,17 +50,38 @@ def apply_louvain(g):
 
     coms_louvain = [tuple(c) for c in coms_to_node.values()]
 
+    write_results('louvain', subsize,
+                  str(pquality.pquality_summary(g, coms_louvain)))
     return pquality.pquality_summary(g, coms_louvain)
 
 
-def apply_labelprop(g):
+def apply_labelprop(g, subsize=1000):
     print 'COMPUTING LABEL PROPAGATION SCORE'
 
     g = g.to_undirected()
     lp = list(community.label_propagation_communities(g))
     lp = [tuple(x) for x in lp]
 
+    write_results('label_propagation', subsize,
+                  pquality.pquality_summary(g, lp))
     return pquality.pquality_summary(g, lp)
+
+
+def write_results(algorithm, subsize, results, k=0):
+    f = open('./results/' + algorithm + '_results.txt', 'w')
+    f.write('Subsize: ' + str(subsize) + '\n\n')
+
+    if algorithm == 'girvan_newman':
+        for res in results:
+            f.write('Iteration: ' + str(res) + '\n')
+            f.write(str(results[res]))
+            f.write('\n\n')
+    elif algorithm == 'k_clique':
+        f.write('k: ' + str(k) + '\n\n')
+        f.write(str(results))
+    else:
+        f.write(str(results))
+    f.close()
 
 
 if __name__ == '__main__':
@@ -68,26 +92,31 @@ if __name__ == '__main__':
     alg = raw_input('ALGORITHM TO APPLY(gn/kclique/louvain/demon/labelprop/'
                     'end): ')
     nodes = list(g.nodes())
-    randoms = [random.randint(0, len(nodes) - 1) for i in range(1000)]
+    subsize = 1000
+    randoms = [random.randint(0, len(nodes) - 1) for i in range(subsize)]
     res_gn, res_kclique, res_louvain, res_lab = None, None, None, None
 
     while alg != 'end':
         if alg == 'gn':
-            res_gn = apply_gn(g.subgraph([nodes[r] for r in randoms]))
+            res_gn = apply_gn(g.subgraph([nodes[r] for r in randoms]),
+                              ntimes=5, subsize=subsize)
         elif alg == 'kclique':
             res_kclique = apply_kclique(
-                                    g.subgraph([nodes[r] for r in randoms]))
+                                    g.subgraph([nodes[r] for r in randoms]),
+                                    subsize=subsize)
         elif alg == 'louvain':
             res_louvain = apply_louvain(
-                                    g.subgraph([nodes[r] for r in randoms]))
+                                    g.subgraph([nodes[r] for r in randoms]),
+                                    subsize=subsize)
         elif alg == 'labelprop':
-            res_lab = apply_labelprop(g.subgraph([nodes[r] for r in randoms]))
+            res_lab = apply_labelprop(g.subgraph([nodes[r] for r in randoms]),
+                                      subsize=subsize)
         else:
             pass
 
         alg = raw_input('ALGORITHM TO APPLY(gn/kclique/louvain/demon/'
                         'labelprop/end): ')
 
-    nf = NF1(res_lab, res_louvain)
+    # nf = NF1(res_lab, res_louvain)
 
-    print nf.summary()
+    # print nf.summary()
