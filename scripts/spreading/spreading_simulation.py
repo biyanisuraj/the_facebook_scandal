21ef0978e1771e95bb374a6e0ff884768d3a8f3d
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2018-06-06 17:46:52 +0200

    Added other netwroks to spreading script

--- scripts/spreading/spreading_simulation.py
+++ scripts/spreading/spreading_simulation.py
@@ -6,39 +6,52 @@ import ndlib.models.ModelConfig as mc
 import networkx as nx
 from ndlib.viz.mpl.DiffusionPrevalence import DiffusionPrevalence
 from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
+from ndlib.viz.mpl.TrendComparison import DiffusionTrendComparison
 
 print '\nIMPORTING NETWORKS'
 
 g = nx.read_edgelist('../network/networks/edge_list.txt',
                      create_using=nx.DiGraph(), nodetype=int, data=False)
-# er_g = nx.read_edgelist('../network/networks/er_edge_list.txt',
-#                         create_using=nx.DiGraph(), nodetype=int, data=False)
-# ba_g = nx.read_edgelist('../network/networks/ba_edge_list.txt',
-#                         create_using=nx.Graph(), nodetype=int, data=False)
+er_g = nx.read_edgelist('../network/networks/er_edge_list.txt',
+                        create_using=nx.DiGraph(), nodetype=int, data=False)
+ba_g = nx.read_edgelist('../network/networks/ba_edge_list.txt',
+                        create_using=nx.Graph(), nodetype=int, data=False)
 
 print '\nSI MODEL SIMULATION'
-
-model_si = si.SIModel(g)
-
 print 'MODEL CONFIGURATION'
 
 cfg = mc.Configuration()
 cfg.add_model_parameter('beta', float(raw_input('INFECTION RATE: ')))
 cfg.add_model_parameter('percentage_infected', 0.05)
-model_si.set_initial_status(cfg)
 
+model_si = si.SIModel(g)
+model_si.set_initial_status(cfg)
 iterations = model_si.iteration_bunch(200)
 trends_si = model_si.build_trends(iterations)
-
 viz = DiffusionTrend(model_si, trends_si)
-viz.plot('si_diffusion.pdf')
+viz.plot('./visuals/si/diffusion.pdf')
 viz = DiffusionPrevalence(model_si, trends_si)
-viz.plot('si_prevalence.pdf')
+viz.plot('./visuals/si/prevalence.pdf')
+
+model_si = si.SIModel(er_g)
+model_si.set_initial_status(cfg)
+iterations = model_si.iteration_bunch(200)
+trends_si = model_si.build_trends(iterations)
+viz = DiffusionTrend(model_si, trends_si)
+viz.plot('./visuals/si/diffusion_er.pdf')
+viz = DiffusionPrevalence(model_si, trends_si)
+viz.plot('./visuals/si/prevalence_er.pdf')
+
+model_si = si.SIModel(ba_g)
+model_si.set_initial_status(cfg)
+iterations = model_si.iteration_bunch(200)
+trends_si = model_si.build_trends(iterations)
+viz = DiffusionTrend(model_si, trends_si)
+viz.plot('./visuals/si/diffusion_ba.pdf')
+viz = DiffusionPrevalence(model_si, trends_si)
+viz.plot('./visuals/si/prevalence_ba.pdf')
 
 print '\nSIR MODEL SIMULATION'
-
-model_sir = sir.SIRModel(g)
-
 print 'MODEL CONFIGURATION'
 
 cfg = mc.Configuration()
@@ -46,20 +59,35 @@ cfg.add_model_parameter('beta', float(raw_input('INFECTION RATE: ')))
 cfg.add_model_parameter('gamma', float(raw_input('RECOVERY RATE: ')))
 cfg.add_model_parameter('percentage_infected',
                         float(raw_input('PERCENTAGE INFECTED: ')))
-model_sir.set_initial_status(cfg)
 
+model_sir = sir.SIRModel(g)
+model_sir.set_initial_status(cfg)
 iterations = model_sir.iteration_bunch(200)
 trends_sir = model_sir.build_trends(iterations)
-
 viz = DiffusionTrend(model_sir, trends_sir)
-viz.plot("sir_diffusion.pdf")
+viz.plot("./visuals/sir/diffusion.pdf")
 viz = DiffusionPrevalence(model_sir, trends_sir)
-viz.plot('sir_prevalence.pdf')
+viz.plot('./visuals/sir/prevalence.pdf')
+
+model_sir = sir.SIRModel(er_g)
+model_sir.set_initial_status(cfg)
+iterations = model_sir.iteration_bunch(200)
+trends_sir = model_sir.build_trends(iterations)
+viz = DiffusionTrend(model_sir, trends_sir)
+viz.plot("./visuals/sir/diffusion_er.pdf")
+viz = DiffusionPrevalence(model_sir, trends_sir)
+viz.plot('./visuals/sir/prevalence_er.pdf')
+
+model_sir = sir.SIRModel(ba_g)
+model_sir.set_initial_status(cfg)
+iterations = model_sir.iteration_bunch(200)
+trends_sir = model_sir.build_trends(iterations)
+viz = DiffusionTrend(model_sir, trends_sir)
+viz.plot("./visuals/sir/diffusion_ba.pdf")
+viz = DiffusionPrevalence(model_sir, trends_sir)
+viz.plot('./visuals/sir/prevalence_ba.pdf')
 
 print '\nSIS MODEL SIMULATION'
-
-model_sis = sis.SISModel(g)
-
 print 'MODEL CONFIGURATION'
 
 cfg = mc.Configuration()
@@ -67,15 +95,33 @@ cfg.add_model_parameter('beta', float(raw_input('INFECTION RATE: ')))
 cfg.add_model_parameter('lambda', float(raw_input('RECOVERY PROBABILITY: ')))
 cfg.add_model_parameter('percentage_infected',
                         float(raw_input('PERCENTAGE INFECTED: ')))
-model_sis.set_initial_status(cfg)
 
+model_sis = sis.SISModel(g)
+model_sis.set_initial_status(cfg)
 iterations = model_sis.iteration_bunch(200)
 trends_sis = model_sis.build_trends(iterations)
-
 viz = DiffusionTrend(model_sis, trends_sis)
-viz.plot("sis_diffusion.pdf")
+viz.plot("./visuals/sis/diffusion.pdf")
 viz = DiffusionPrevalence(model_sis, trends_sis)
-viz.plot('sis_prevalence.pdf')
+viz.plot('./visuals/sis/prevalence.pdf')
+
+model_sis = sis.SISModel(er_g)
+model_sis.set_initial_status(cfg)
+iterations = model_sis.iteration_bunch(200)
+trends_sis = model_sis.build_trends(iterations)
+viz = DiffusionTrend(model_sis, trends_sis)
+viz.plot("./visuals/sis/diffusion_er.pdf")
+viz = DiffusionPrevalence(model_sis, trends_sis)
+viz.plot('./visuals/sis/prevalence_er.pdf')
+
+model_sis = sis.SISModel(ba_g)
+model_sis.set_initial_status(cfg)
+iterations = model_sis.iteration_bunch(200)
+trends_sis = model_sis.build_trends(iterations)
+viz = DiffusionTrend(model_sis, trends_sis)
+viz.plot("./visuals/sis/diffusion_ba.pdf")
+viz = DiffusionPrevalence(model_sis, trends_sis)
+viz.plot('./visuals/sis/prevalence_ba.pdf')
 
 print '\nTHRESHOLD MODEL SIMULATION'
 
@@ -97,6 +143,11 @@ iterations = model_thr.iteration_bunch(200)
 trends_thr = model_thr.build_trends(iterations)
 
 viz = DiffusionTrend(model_thr, trends_thr)
-viz.plot("thr_diffusion.pdf")
+viz.plot("./visuals/threshold/diffusion.pdf")
 viz = DiffusionPrevalence(model_thr, trends_thr)
-viz.plot('thr_prevalence.pdf')
+viz.plot('./visuals/threshold/prevalence.pdf')
+
+# viz = DiffusionTrend([model_si, model_sis, model_sir, model_thr],
+#                      [trends_si, trends_sis, trends_sir, trends_thr]
+#                      )
+# viz.plot("trend_comparison.pdf")
