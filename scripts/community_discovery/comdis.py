6a86a3bdbe6f4a11c20752b58d2b126ff7f6e2fc
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2018-06-10 11:45:48 +0200

    Update

--- scripts/community_discovery/comdis.py
+++ scripts/community_discovery/comdis.py
@@ -55,6 +55,7 @@ def evaluate_partition(infos):
 def apply_kclique(g, subsize=1000):
     print 'COMPUTING K-CLIQUE SCORE'
     g = g.to_undirected()
+    partitions = dict()
 
     for k in [3, 4, 5]:
         kclique = list(community.k_clique_communities(g, k))
@@ -72,6 +73,9 @@ def apply_kclique(g, subsize=1000):
             extract_info(max_community)
             evaluate_partition({'alg': 'k-clique', 'network': g, 'k': k,
                                'partition': kclique})
+            partitions[k] = kclique
+
+    return partitions
 
 
 def apply_labelprop(g, subsize=1000):
@@ -92,6 +96,8 @@ def apply_labelprop(g, subsize=1000):
         extract_info(max_community)
         evaluate_partition({'alg': 'label propagation', 'network': g,
                            'partition': lp})
+
+        return lp
 
 
 def apply_louvain(g, subsize=1000):
@@ -118,6 +124,8 @@ def apply_louvain(g, subsize=1000):
         evaluate_partition({'alg': 'louvain', 'network': g,
                            'partition': coms_louvain})
 
+        return coms_louvain
+
 
 def apply_gn(g, subsize=1000):
     print 'COMPUTING GIRVAN-NEWMAN SCORE'
@@ -140,6 +148,8 @@ def apply_gn(g, subsize=1000):
     evaluate_partition({'alg': 'girvan-newman', 'network': g,
                        'partition': iterations})
 
+    return iterations
+
 
 def apply_demon(g, subsize=1000):
     print 'COMPUTING DEMON SCORE'
@@ -157,6 +167,8 @@ def apply_demon(g, subsize=1000):
     evaluate_partition({'alg': 'demon', 'network': g,
                        'partition': coms_demon})
 
+    return coms_demon
+
 
 if __name__ == '__main__':
     print 'IMPORTING NETWORK\n'
@@ -167,61 +179,119 @@ if __name__ == '__main__':
     subsize = 1000
     randoms = [random.randint(0, len(nodes) - 1) for i in range(subsize)]
 
-    apply_kclique(g.subgraph([nodes[r] for r in randoms]), subsize=subsize)
+    p_kclique = apply_kclique(g.subgraph([nodes[r] for r in randoms]),
+                              subsize=subsize)
     print '\n'
-    apply_labelprop(g.subgraph([nodes[r] for r in randoms]), subsize=subsize)
+    p_labelprop = apply_labelprop(g.subgraph([nodes[r] for r in randoms]),
+                                  subsize=subsize)
     print '\n'
-    apply_louvain(g.subgraph([nodes[r] for r in randoms]), subsize=subsize)
+    p_louvain = apply_louvain(g.subgraph([nodes[r] for r in randoms]),
+                              subsize=subsize)
     print '\n'
-    apply_gn(g.subgraph([nodes[r] for r in randoms]), subsize=subsize)
+    p_gn = apply_gn(g.subgraph([nodes[r] for r in randoms]), subsize=subsize)
     print '\n'
-    apply_demon(g.subgraph([nodes[r] for r in randoms]), subsize=subsize)
+    p_demon = apply_demon(g.subgraph([nodes[r] for r in randoms]),
+                          subsize=subsize)
 
-    # r_gn = r_gn[max(r_gn.keys())]['Communities']
-    # res = [r_gn, r_kclique, r_louvain, r_lab, r_demon]
+    ps = [p_kclique, p_labelprop, p_louvain, p_gn, p_demon]
 
-    # for i in range(len(res)):
-    #     for j in range(i + 1, len(res)):
-    #         if res[i] == r_gn:
-    #             if res[j] == r_kclique:
-    #                 f = open('./comparisons/gn_kclique.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #             elif res[j] == r_louvain:
-    #                 f = open('./comparisons/gn_louvain.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #             elif res[j] == r_lab:
-    #                 f = open('./comparisons/gn_labelprop.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #             else:
-    #                 f = open('./comparisons/gn_demon.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #         elif res[i] == r_kclique:
-    #             if res[j] == r_louvain:
-    #                 f = open('./comparisons/kclique_louvain.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #             elif res[j] == r_lab:
-    #                 f = open('./comparisons/kclique_labelprop.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #             else:
-    #                 f = open('./comparisons/kclique_demon.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #         elif res[i] == r_louvain:
-    #             if res[j] == r_lab:
-    #                 f = open('./comparisons/louvain_labelprop.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #             else:
-    #                 f = open('./comparisons/louvain_demon.txt', 'w')
-    #                 f.write(str(NF1(res[i], res[j]).summary()))
-    #                 f.close()
-    #         elif res[i] == r_lab:
-    #             f = open('./comparisons/labelprop_demon.txt', 'w')
-    #             f.write(str(NF1(res[i], res[j]).summary()))
-    #             f.close()
+    for i in range(len(ps)):
+        for j in range(i + 1, len(ps)):
+            if ps[i] == p_kclique:
+                for k in p_kclique:
+                    if ps[j] == p_labelprop:
+                        comp = NF1(ps[i][k], ps[j]).summary()
+                        comp['scores'].to_csv(path_or_buf='./comparisons/' +
+                                              'k_clique/k_clique_' + str(k) +
+                                              '_labelprop_scores.csv')
+                        comp['details'].to_csv(path_or_buf='./comparisons/' +
+                                               'k_clique/k_clique_' + str(k) +
+                                               '_labelprop_details.csv')
+                    elif ps[j] == p_louvain:
+                        comp = NF1(ps[i][k], ps[j]).summary()
+                        comp['scores'].to_csv(path_or_buf='./comparisons/' +
+                                              'k_clique/k_clique_' + str(k) +
+                                              '_louvain_scores.csv')
+                        comp['details'].to_csv(path_or_buf='./comparisons/' +
+                                               'k_clique/k_clique_' + str(k) +
+                                               '_louvain_details.csv')
+                    elif ps[j] == p_gn:
+                        for iteration in ps[j]:
+                            comp = NF1(ps[i][k], ps[j][iteration]).summary()
+                            comp['scores'].to_csv(path_or_buf='./comparisons/'
+                                                  + 'k_clique/k_clique_' +
+                                                  str(k) + '_gn_it_' +
+                                                  str(iteration) +
+                                                  '_scores.csv')
+                            comp['details'].to_csv(path_or_buf='./comparisons/'
+                                                   + 'k_clique/k_clique_' +
+                                                   str(k) + '_gn_it_' +
+                                                   str(iteration) +
+                                                   '_details.csv')
+                    else:
+                        comp = NF1(ps[i][k], ps[j]).summary()
+                        comp['scores'].to_csv(path_or_buf='./comparisons/' +
+                                              'k_clique/k_clique_' + str(k) +
+                                              '_demon_scores.csv')
+                        comp['details'].to_csv(path_or_buf='./comparisons/' +
+                                               'k_clique/k_clique_' + str(k) +
+                                               '_demon_details.csv')
+            elif ps[i] == p_labelprop:
+                if ps[j] == p_louvain:
+                    comp = NF1(ps[i], ps[j]).summary()
+                    comp['scores'].to_csv(path_or_buf='./comparisons/' +
+                                          'label_propagation/label_' +
+                                          'propagation_louvain_scores.csv')
+                    comp['details'].to_csv(path_or_buf='./comparisons/' +
+                                           'label_propagation/label_' +
+                                           'propagation_louvain_details.csv')
+                elif ps[j] == p_gn:
+                    for iteration in ps[j]:
+                        comp = NF1(ps[i], ps[j][iteration]).summary()
+                        comp['scores'].to_csv(path_or_buf='./comparisons/'
+                                              + 'label_propagation/label_' +
+                                              'propagation_gn_it_' +
+                                              str(iteration) +
+                                              '_scores.csv')
+                        comp['details'].to_csv(path_or_buf='./comparisons/'
+                                               + 'label_propagation/label_' +
+                                               'propagation_gn_it_' +
+                                               str(iteration) +
+                                               '_details.csv')
+                else:
+                    comp = NF1(ps[i], ps[j]).summary()
+                    comp['scores'].to_csv(path_or_buf='./comparisons/' +
+                                          'label_propagation/label_' +
+                                          'propagation_demon_scores.csv')
+                    comp['details'].to_csv(path_or_buf='./comparisons/' +
+                                           'label_propagation/label_' +
+                                           'propagation_demon_details.csv')
+            elif ps[i] == p_louvain:
+                if ps[j] == p_gn:
+                    for iteration in ps[j]:
+                        comp = NF1(ps[i], ps[j][iteration]).summary()
+                        comp['scores'].to_csv(path_or_buf='./comparisons/'
+                                              + 'louvain/louvain_gn_it_' +
+                                              str(iteration) +
+                                              '_scores.csv')
+                        comp['details'].to_csv(path_or_buf='./comparisons/'
+                                               + 'louvain/louvain_gn_it_' +
+                                               str(iteration) +
+                                               '_details.csv')
+                else:
+                    comp = NF1(ps[i], ps[j]).summary()
+                    comp['scores'].to_csv(path_or_buf='./comparisons/'
+                                          + 'louvain/louvain_demon_scores.csv')
+                    comp['details'].to_csv(path_or_buf='./comparisons/'
+                                           + 'louvain/louvain_demon_details' +
+                                           '.csv')
+            elif ps[i] == p_gn:
+                for iteration in ps[i]:
+                    comp = NF1(ps[i][iteration], ps[j]).summary()
+                    comp['scores'].to_csv(path_or_buf='./comparisons/' +
+                                          'girvan_newman/gn_it_' +
+                                          str(iteration) + '_demon_scores.csv')
+                    comp['details'].to_csv(path_or_buf='./comparisons/' +
+                                           'girvan_newman/gn_it_' +
+                                           str(iteration) + '_demon_details' +
+                                           '.csv')
