55defad4c8ef76eb324c5d0d28c837c84c265dd2
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2018-06-14 12:26:34 +0200

    Update

--- scripts/tie_strength/tie_strength_analysis.py
+++ scripts/tie_strength/tie_strength_analysis.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import operator
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -46,5 +47,19 @@ if __name__ == '__main__':
 
     g = nx.read_edgelist('../network/networks/edge_list.txt',
                          create_using=nx.DiGraph(), nodetype=int, data=False)
+
+    nodes, degrees = list(g.nodes()), [d[1] for d in list(g.degree())]
+    k_max, k_min = max(degrees), min(degrees)
+    gamma = 2.6
+    C = 1 / sum([d**(-gamma) for d in degrees])
+
+    fc = 1 - (1/((((gamma - 2)/(3 - gamma)) * k_min**(gamma - 2) *
+              k_max**(3 - gamma)) - 1))
+
+    print 'THE CRITICAL THRESHOLD FOR THE NETWORK IS ' + str(fc)
+
+    fc = 1 - (C / len(nodes)**((3 - gamma)/(gamma - 1)))
+
+    print 'THE CRITICAL THRESHOLD BASED ON THE NETWORK SIZE IS ' + str(fc)
 
     test_cc(g.to_undirected(), ['BETWEENNESS CENTRALITY'], 50)
